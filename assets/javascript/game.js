71c4321546e76d00330af8bff0d013a5ae06a3c5
shaanobney <shaan.obney@gmail.com>
2016-07-20 21:49:44 -0700

    changed fonts and layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,55 @@
 var wordBank = ["burzum", "emperor", "darkthrone", "immortal", "marduk", "venom", "bathory", "obituary", "dismember", "hellhammer", "suffocation", "entombed", "nile", "gorgoroth", "mayhem", "death", "deicide", "brujeria", "carcass", "deafheaven"]
-var word = new Array(); //HOLDS SUBSTRINGS OF LETTERS FROM WORD.
+var word = new Array(); //HOLDS SUBSTRINGS OF LETTERS FROM WORDBANK
 var wordLength;
-var totAttempt;
-var audio = new Audio('luna.mp3'); //AUDIO FOR WINS/LOSSES.
-var audios = new Audio('lose.mp3') //SHOULD HAVED NAMED THIS ADIOS.
+var totLives;
+var audio = new Audio('luna.mp3'); //AUDIO FOR WINS/LOSSES
+var audios = new Audio('lose.mp3');
 
 
-
-//STARTS NEW ROUND. HAD TO SCRUB WINS/LOSSES FROM IT DUE TO BEING A CLOWN.
-function newRound()
-{
-	document.getElementById(enableKeys()); //RESET
-	document.getElementById('attempts').innerHTML = "Lives Left: " + (totAttempt = 7);
-	var num = (Math.random() * wordBank.length); //MATH FOR WORD SELECTION.
-	var num = Math.round(num);	//ROUNDING THE RESULT.
-	word = wordBank[num].split(""); //SPLITS WORD INTO ARRAY OF ITS LETTERS.
+//STARTS NEW ROUND. HAD TO SCRUB WINS/LOSSES FROM IT DUE TO BEING A CLOWN
+function newRound() {
+	document.getElementById(enableKeys()); //RESETS KEY STATE
+	document.getElementById('attempts').innerHTML = "Lives Left: " + (totLives = 7);
+	var num = (Math.random() * wordBank.length); //MATH FOR WORD SELECTION
+	var num = Math.round(num);	//ROUNDING THE RESULT
+	word = wordBank[num].split(""); //SPLITS WORD INTO ARRAY OF ITS LETTERS
 	wordLength = new Array(word.length); 
 	createWord(wordLength);
-	document.getElementById("keyboard").reset(); //ANOTHER RESET. LAST BUTTON PRESSED WOULD ALWAYS RETURN TO A DISABLED STATE ON NEW ROUND WITHOUT THIS.
-}
+	document.getElementById("keyboard").reset(); //ANOTHER RESET. LAST BUTTON PRESSED WOULD ALWAYS RETURN TO A DISABLED STATE ON NEW ROUND WITHOUT THIS
+};
 
-
-//SETS INITIAL CONDITIONS AT START OF FRESH GAME / RESETS THE GAME VIA RESET BUTTON. SHOULD BE MUCH DIFFERENT FROM ABOVE FUNCTION.
-function setCond()
-{
+//SETS INITIAL CONDITIONS AT START OF FRESH GAME / RESETS THE GAME VIA RESET BUTTON. SHOULD BE MUCH DIFFERENT FROM ABOVE FUNCTION
+function setCond() {
 	document.getElementById(enableKeys());
-	document.getElementById('attempts').innerHTML = "Lives Left: " + (totAttempt = 7);
+	document.getElementById('attempts').innerHTML = "Lives Left: " + (totLives = 7);
 	document.getElementById('myLives').innerHTML = " ";
 	var num = (Math.random() * wordBank.length); 
-	var num = parseInt(num);	
+	var num = Math.round(num);	
 	word = wordBank[num].split("");
 	wordLength = new Array(word.length);
 	createWord(wordLength);
-}
-
-//
+};
+//MAIN GAME LOGIC. LETTER VALUE ATTEMPTED BY USER IS INPUTED VIA RADIO BUTTON. ANSWER IS EITHER TRUE OR FALSE TO CONDITIONS EFFECTING GAME
 function letter(value) {
-	var attempt = false;
+	var choice = false;
 	for (var i = 0; i < wordBank.length; i++) {
-		if (value == word[i])
-		{
+		if (value == word[i]) {
 			wordLength[i] = value;
-			attempt = true;
+			choice = true;
 		}
 	}	
 
-	if (attempt == true) {
-		if (wordLength.join() == word.join() && totAttempt < 8 && totAttempt > 0) {
+	if (choice == true) {
+		if (wordLength.join() == word.join() && totLives < 8 && totLives > 0) {
 			document.getElementById('myLives').innerHTML = "You Have Vanquished the Weak and Torn the Earth Asunder (You Win!)";
 			audio.play();
 			newRound();
 		}
-		
 	}
 	
-	if (attempt == false) {
-		totAttempt--;
-		if (totAttempt == 0) {
+	if (choice == false) {
+		totLives--;
+		if (totLives == 0) {
 			document.getElementById('myLives').innerHTML = "You have been turned into a dessicated pile of bones (You Lose!)";
 			disableKeys();
 			audios.play();
@@ -68,13 +61,11 @@ function letter(value) {
 			}	
 		}
 	}
-	
-	
-	
+		
 	createWord(wordLength);
-	document.getElementById('attempts').innerHTML = "Attempts Left: " + totAttempt;
-}
-
+	document.getElementById('attempts').innerHTML = "Lives Left: " + totLives;
+};
+//DETERMINES WHETHER RESULT IS A LETTER OR AN UNDERSCORE
 function createWord(word) {
 	var result="";
 	var solution = result;
@@ -87,64 +78,63 @@ function createWord(word) {
 		}	
 		document.getElementById("answer").innerHTML=result;
 	}		
-}
+};
 
-
-//INTEGRATED THIS KEYBOARD WHILE I WAS TRYING TO FIGURE OUT KEY LISTENERS, THEN RAN OUT OF TIME AND COULDN'T GET RID OF IT.
+//DISABLES OR ENABLES RADIO BUTTONS BASED ON GAME STATE OR LETTERS USED
 function disableKeys() {	
-	document.getElementById("q").disabled=true;
-	document.getElementById("w").disabled=true;
-	document.getElementById("e").disabled=true;
-	document.getElementById("r").disabled=true;
-	document.getElementById("t").disabled=true;
-	document.getElementById("y").disabled=true;
-	document.getElementById("u").disabled=true;
-	document.getElementById("i").disabled=true;
-	document.getElementById("o").disabled=true;
-	document.getElementById("p").disabled=true;
 	document.getElementById("a").disabled=true;
-	document.getElementById("s").disabled=true;
+	document.getElementById("b").disabled=true;
+	document.getElementById("c").disabled=true;
 	document.getElementById("d").disabled=true;
+	document.getElementById("e").disabled=true;
 	document.getElementById("f").disabled=true;
 	document.getElementById("g").disabled=true;
 	document.getElementById("h").disabled=true;
+	document.getElementById("i").disabled=true;
 	document.getElementById("j").disabled=true;
 	document.getElementById("k").disabled=true;
 	document.getElementById("l").disabled=true;
-	document.getElementById("z").disabled=true;
-	document.getElementById("x").disabled=true;
-	document.getElementById("c").disabled=true;
-	document.getElementById("v").disabled=true;
-	document.getElementById("b").disabled=true;
+	document.getElementById("m").disabled=true;
 	document.getElementById("n").disabled=true;
-	document.getElementById("m").disabled=true;	
-}
+	document.getElementById("o").disabled=true;
+	document.getElementById("p").disabled=true;
+	document.getElementById("q").disabled=true;
+	document.getElementById("r").disabled=true;
+	document.getElementById("s").disabled=true;
+	document.getElementById("t").disabled=true;
+	document.getElementById("u").disabled=true;
+	document.getElementById("v").disabled=true;
+	document.getElementById("w").disabled=true;
+	document.getElementById("x").disabled=true;
+	document.getElementById("y").disabled=true;
+	document.getElementById("z").disabled=true;	
+};
 
 function enableKeys() {	
-	document.getElementById("q").disabled=false;
-	document.getElementById("w").disabled=false;
-	document.getElementById("e").disabled=false;
-	document.getElementById("r").disabled=false;
-	document.getElementById("t").disabled=false;
-	document.getElementById("y").disabled=false;
-	document.getElementById("u").disabled=false;
-	document.getElementById("i").disabled=false;
-	document.getElementById("o").disabled=false;
-	document.getElementById("p").disabled=false;
 	document.getElementById("a").disabled=false;
-	document.getElementById("s").disabled=false;
+	document.getElementById("b").disabled=false;
+	document.getElementById("c").disabled=false;
 	document.getElementById("d").disabled=false;
+	document.getElementById("e").disabled=false;
 	document.getElementById("f").disabled=false;
 	document.getElementById("g").disabled=false;
 	document.getElementById("h").disabled=false;
+	document.getElementById("i").disabled=false;
 	document.getElementById("j").disabled=false;
 	document.getElementById("k").disabled=false;
 	document.getElementById("l").disabled=false;
-	document.getElementById("z").disabled=false;
-	document.getElementById("x").disabled=false;
-	document.getElementById("c").disabled=false;
-	document.getElementById("v").disabled=false;
-	document.getElementById("b").disabled=false;
-	document.getElementById("n").disabled=false;
 	document.getElementById("m").disabled=false;
+	document.getElementById("n").disabled=false;
+	document.getElementById("o").disabled=false;
+	document.getElementById("p").disabled=false;
+	document.getElementById("q").disabled=false;
+	document.getElementById("r").disabled=false;
+	document.getElementById("s").disabled=false;
+	document.getElementById("t").disabled=false;
+	document.getElementById("u").disabled=false;
+	document.getElementById("v").disabled=false;
+	document.getElementById("w").disabled=false;
+	document.getElementById("x").disabled=false;
+	document.getElementById("y").disabled=false;
+	document.getElementById("z").disabled=false;
 }
